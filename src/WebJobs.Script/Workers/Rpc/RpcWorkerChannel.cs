33d1ac120726d29279d8f70cb213c2af00d977ee
Ankit Kumar <ankikuma@microsoft.com>
2020-03-09 01:31:41 -0700

    Make proxy use custom provider, and re-design

--- src/WebJobs.Script/Workers/Rpc/RpcWorkerChannel.cs
+++ src/WebJobs.Script/Workers/Rpc/RpcWorkerChannel.cs
@@ -174,19 +174,12 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc
 
         public void SetupFunctionInvocationBuffers(IEnumerable<FunctionMetadata> functions)
         {
-            var languageFunctions = new List<FunctionMetadata>();
-
+            _functions = functions;
             foreach (FunctionMetadata metadata in functions)
             {
-                if (Utility.IsFunctionMetadataLanguageSupportedByWorkerRuntime(metadata, _workerConfig?.Description?.Language))
-                {
-                    _workerChannelLogger.LogDebug("Setting up FunctionInvocationBuffer for function:{functionName} with functionId:{id}", metadata.Name, metadata.FunctionId);
-                    _functionInputBuffers[metadata.FunctionId] = new BufferBlock<ScriptInvocationContext>();
-                    languageFunctions.Add(metadata);
-                }
+                _workerChannelLogger.LogDebug("Setting up FunctionInvocationBuffer for function:{functionName} with functionId:{id}", metadata.Name, metadata.FunctionId);
+                _functionInputBuffers[metadata.FunctionId] = new BufferBlock<ScriptInvocationContext>();
             }
-
-            _functions = languageFunctions;
         }
 
         public void SendFunctionLoadRequests(ManagedDependencyOptions managedDependencyOptions)
